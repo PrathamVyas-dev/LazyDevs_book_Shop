16e5d31de0af04cddf28d9a7322c938fd6c8f893
Singhal <2401739@cognizant.com>
2025-04-01 18:13:42 +0530

    done the sql

--- Kushagra/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ Kushagra/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("4bef8990-c2a3-4caf-b426-182f9a9e6a12")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7109bcff8197694df47304fa7b10d0b26e0c502")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a56dafdd214f89bccdbbe92aec2f2d2c40a6e46")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
